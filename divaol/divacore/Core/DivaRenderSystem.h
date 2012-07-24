69bae97bcf1223682dedc76efe17d0d431034040
hyf042 <hyf042@gmail.com>
2012-07-24 17:23:21 +0800

    add rendersystem-mask

--- divaol/divacore/Core/DivaRenderSystem.h
+++ divaol/divacore/Core/DivaRenderSystem.h
@@ -24,10 +24,13 @@ namespace divacore
 	class RenderSystem : public EventHandler
 	{
 	public:
+		enum{RS_RENDER_BACKGROUND = 1,RS_RENDER_NOTE = 2,RS_RENDER_UI = 4,RS_RENDER_EFFECT = 8};
+		static const uint32 RS_RENDER_ALL = RS_RENDER_BACKGROUND|RS_RENDER_NOTE|RS_RENDER_UI|RS_RENDER_EFFECT;
+	public:
 		virtual void prepare(Config &config) = 0;
 		virtual void render() = 0;
-		virtual void draw() = 0;
-		virtual sora::SoraSprite* renderToCanvas(float width = 0, float height = 0) = 0;
+		virtual void draw(uint32 mask) = 0;
+		virtual sora::SoraSprite* renderToCanvas(float width = 0, float height = 0, uint32 mask = RS_RENDER_ALL) = 0;
 		virtual void render(sora::SoraSprite *sprite, const std::string &tag = "") = 0;
 		virtual void render(float x, float y, sora::SoraSprite *sprite, const std::string &tag = "") = 0;
 		virtual bool InsideDrawRange(const Point &p) = 0;
