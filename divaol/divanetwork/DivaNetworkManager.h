2c411cee849cffaea86b858c2c5a2d3744dc909e
hyf042 <hyf042@gmail.com>
2012-09-26 12:59:19 +0800

    tiny fix

--- divaol/divanetwork/DivaNetworkManager.h
+++ divaol/divanetwork/DivaNetworkManager.h
@@ -41,6 +41,8 @@ namespace divanet
 		inline NetworkPtr scheduler() {return mScheduler;}
 		void setAuth(NetworkPtr ptr) {mAuth = ptr;}
 		void setChat(NetworkPtr ptr) {mChat = ptr;}
+		void setCore(NetworkPtr ptr) {mCore = ptr;}
+		void setScheduler(NetworkPtr ptr) {mScheduler = ptr;}
 
 		void connectAuth() {
 			mAuth->setHostInfo(info["auth"].ip,info["auth"].port);
@@ -52,6 +54,16 @@ namespace divanet
 			mChat->connect();
 		}
 
+		void connectCore() {
+			mCore->setHostInfo(info["core"].ip,info["core"].port);
+			mCore->connect();
+		}
+
+		void connectScheduler() {
+			mScheduler->setHostInfo(info["scheduler"].ip,info["scheduler"].port);
+			mScheduler->connect();
+		}
+
 		virtual void init() {
 			getServiceInfo();
 		}
@@ -59,6 +71,8 @@ namespace divanet
 		virtual void getServiceInfo() {
 			info["auth"] = ServiceInfo("58.215.170.145",9699);
 			info["chat"] = ServiceInfo("58.215.170.145",9799);
+			info["core"] = ServiceInfo("58.215.170.145",9899);
+			info["scheduler"] = ServiceInfo("58.215.170.145",9099);
 		}
 
 		void onUpdate(float dt) {
@@ -71,6 +85,12 @@ namespace divanet
 				mChat->update(dt);
 				if(!mChat->isConnected())
 					mIsChat = false;
+			}
+			if(mCore&&mCore->isConnected()) {
+				mCore->update(dt);
+			}
+			if(mScheduler&&mScheduler->isConnected()) {
+				mScheduler->update(dt);
 			}
 		}
 
