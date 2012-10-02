605c9ab0afce2b90732673d638bba3b64e1a2492
hyf042 <hyf042@gmail.com>
2012-10-02 17:39:31 +0800

    update StageServer

--- divaol/divanetwork/DivaStageServer.h
+++ divaol/divanetwork/DivaStageServer.h
@@ -13,11 +13,29 @@
 
 namespace divanet
 {
+	struct WaiterInfo {
+		enum State{LEAVE,READY,UNREADY};
+		std::string uid;
+		uint32 status;
+		uint32 color;
+		uint32 slot;
+	};
+	typedef std::vector<WaiterInfo> Waiters;
+	struct StageInfo {
+		enum State{STAGE,GAME};
+		uint32 capacity;
+		std::string owner;
+		std::string mode;
+		uint32 songId;
+		uint32 status;
+		Waiters waiters;
+	};
+
 	class StageClient : public Client, public Base::Singleton<StageClient>
 	{
-		enum StageState{OUTSIDE,STAGE,GAME};
+		enum StageState{OUTSIDE,GETTING_INFO,STAGE,GAME};
 	public:
-		enum NotifyType{NOTIFY_STAGE_JOIN = 0x80,NOTIFY_STAGE_LEAVE,NOTIFY_STAGE_START};
+		enum NotifyType{NOTIFY_STAGE_JOIN = 0x80,NOTIFY_STAGE_LEAVE,NOTIFY_STAGE_START,NOTIFY_UPDATE_INFO};
 
 		virtual std::string name() const {return "stage";}
 
@@ -25,10 +43,12 @@ namespace divanet
 			mNetSys->send("auth#setuid","%S",NET_INFO.uid);
 
 			GNET_RECEIVE_REGISTER(mNetSys,"stage#start",&StageClient::gnet_start);
+			GNET_RECEIVE_REGISTER(mNetSys,"stage#info",&StageClient::gnet_info);
 		}
 
 		void logout() {
 			GNET_RECEIVE_UNREGISTER(mNetSys,"stage#start");
+			GNET_RECEIVE_UNREGISTER(mNetSys,"stage#info");
 		}
 
 		void create(int capacity) {
@@ -48,6 +68,14 @@ namespace divanet
 			}
 		}
 
+		void draw(uint32 color) {
+
+		}
+
+		void changeSlot(uint32 slot) {
+
+		}
+
 		bool start() {
 			if(state()==STAGE&&owner()) {
 				mNetSys->send("stage#start");
@@ -64,10 +92,12 @@ namespace divanet
 			return mIsOwner;
 		}
 
+		const StageInfo& info() const {return mInfo;}
+
 	public:
 		void gnet_join(GPacket *packet) {
 			if(packet->getItem(2)->getString()=="ok") {
-				mState = true;
+				mState = GETTING_INFO;
 				if(packet->getItem(3)->getString()==NET_INFO.uid)
 					mIsOwner = true;
 			}
@@ -87,6 +117,50 @@ namespace divanet
 			notify("start", NOTIFY_STAGE_START, packet);
 		}
 
+		void gnet_info(GPacket *packet) {
+			GPacket *members = packet->getItem(2)->as<GPacket>();
+			gnet::Item<gnet::Tuple> *stageInfo = packet->getItem(3)->as<gnet::Item<gnet::Tuple>>();
+
+			mInfo.owner = stageInfo->getItem(0)->getString();
+			mInfo.capacity = stageInfo->getItem(1)->getInt();
+			mInfo.songId = stageInfo->getItem(2)->getInt();
+			mInfo.mode = stageInfo->getItem(3)->getString();
+			mInfo.status = stageInfo->getItem(4)->getString()=="STAGE"?StageInfo::STAGE:StageInfo::GAME;
+
+			mInfo.waiters.clear();
+			for(int i = 0; i < members->size(); i++) {
+				gnet::Item<gnet::Tuple> *single = members->getItem(i)->as<gnet::Item<gnet::Tuple>>();
+				WaiterInfo waiter;
+				std::string status = single->getItem(1)->getString();
+				if(status=="LEAVE")
+					waiter.status = WaiterInfo::LEAVE;
+				else if(status=="UNREADY")
+					waiter.status = WaiterInfo::UNREADY;
+				else if(status=="READY")
+					waiter.status = WaiterInfo::READY;
+				else
+					DIVA_EXCEPTION_MESSAGE("unexpected status");
+
+				waiter.color = single->getItem(2)->getInt();
+				waiter.slot = single->getItem(3)->getInt();
+				if(waiter.status!=WaiterInfo::LEAVE)
+					waiter.uid = single->getItem(0)->getString();
+				else
+					waiter.uid = "0";
+
+				mInfo.waiters.push_back(waiter);
+			}
+
+			if(mState==GETTING_INFO) {
+				mState = STAGE;
+			}
+			else if(mState==STAGE) {
+
+			}
+
+			notify("update", NOTIFY_UPDATE_INFO, packet);
+		}
+
 	protected:
 		friend class Base::Singleton<StageClient>;
 
@@ -95,6 +169,7 @@ namespace divanet
 
 	private:
 		bool mIsOwner;
+		StageInfo mInfo;
 		uint32 mState;
 	};
 
