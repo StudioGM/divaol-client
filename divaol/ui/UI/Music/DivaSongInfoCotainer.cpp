81d7ef392462fc173bde302308482820d4a24358
DarkMagicCK <DarkMagicCK@gamemastercn.com>
2013-01-28 14:28:43 +0800

    change song info showment

--- divaol/ui/UI/Music/DivaSongInfoCotainer.cpp
+++ divaol/ui/UI/Music/DivaSongInfoCotainer.cpp
@@ -30,51 +30,63 @@ namespace diva
 			Font* font = getFont();
 			graphics->setFont(font);
 		
-			int h = getHeight() / 5;
+			int h = getHeight() / 9;
 		
+			int x = 40;
+			int ydelta = 10;
+
 			wchar_t temp[100];
 			// Name
 			wsprintf(temp, L"歌曲名 %s", mapInfo.header.name.c_str());
-			graphics->drawTextW(temp, 30, h * 1 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 1 + ydelta);
 
 			// Artist
 			if (mapInfo.header.artists.size() == 0)
 				wsprintf(temp, L"作曲 未知");
 			else
 				wsprintf(temp, L"作曲 %s", mapInfo.header.artists[0].c_str());
-			graphics->drawTextW(temp, 30, h * 2 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 2 + ydelta);
 
 			// Lyric
 			if (mapInfo.header.lyricists.size() == 0)
 				wsprintf(temp, L"作词 未知");
 			else
 				wsprintf(temp, L"作词 %s", mapInfo.header.lyricists[0].c_str());
-			graphics->drawTextW(temp, 200, h * 2 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 3 + ydelta);
 
 			// Lyric
 			if (mapInfo.header.composers.size() == 0)
 				wsprintf(temp, L"编曲 未知");
 			else
 				wsprintf(temp, L"编曲 %s", mapInfo.header.composers[0].c_str());
-			graphics->drawTextW(temp, 310, h * 2 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 4 + ydelta);
 
 			// noter
 			if (mapInfo.header.noters.size() == 0)
 				wsprintf(temp, L"Noter 未知");
 			else
 				wsprintf(temp, L"Noter %s", mapInfo.header.noters[0].c_str());
-			graphics->drawTextW(temp, 30, h * 3 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 5 + ydelta);
 
 			// BPM
 			wsprintf(temp, L"BPM %d", mapInfo.header.bpm);
-			graphics->drawTextW(temp, 200, h * 3 - font->getHeight() / 2);
+			graphics->drawTextW(temp, x, h * 6 + ydelta);
 
 			// VOCOLOID
 			if (mapInfo.header.vocaloids.size() == 0)
 				wsprintf(temp, L"使用VOCALOID  未知");
 			else
-				wsprintf(temp, L"使用VOCALOID  %s", mapInfo.header.vocaloids[0].c_str());
-			graphics->drawTextW(temp, 310, h * 3 - font->getHeight() / 2);
+			{
+				std::wstring vocaloidStr = L"";
+				for(int i=0;i<mapInfo.header.vocaloids.size();i++)
+				{
+					if(vocaloidStr!=L"")
+						vocaloidStr += L"&";
+					vocaloidStr += mapInfo.header.vocaloids[i];
+				}
+				wsprintf(temp, L"使用VOCALOID  %s", vocaloidStr.c_str());
+			}
+			graphics->drawTextW(temp, x, h * 7 + ydelta);
 
 			////LINK
 			//if (mapInfo.directLinkURLs.size() == 0)
