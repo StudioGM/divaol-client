2f16e9d88f46e7e5d2e5238b798684a0a1b73358
hyf042 <hyf042@gmail.com>
2015-04-05 20:22:18 +0800

    update to vs2013

--- divaol/divapomelo/diva/NetInfo.h
+++ divaol/divapomelo/diva/NetInfo.h
@@ -41,7 +41,7 @@ namespace divapomelo
 	public:
 		ServerInfo() {
 			gatePort = 3014;
-			gateIp = "divaolserver00.gmgate.net";
+			gateIp = "10.155.0.198";
 			//gateIp = "127.0.0.1";
 		}
 		~ServerInfo() {}
